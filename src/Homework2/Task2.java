69550886a4f3405d76923d2a728c6abb573fd293
posthak <adler_m@mail.ru>
2023-08-04 01:24:53 +0200

    Commit Homework2 task 1

--- src/Homework2/Task2.java
+++ src/Homework2/Task2.java
@@ -1,5 +1,22 @@
 package Homework2;
 
-public class Task2 {
+import java.util.Scanner;
 
+// Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), 
+// и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения, 
+// вместо этого, необходимо повторно запросить у пользователя ввод данных.
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            try {
+                double data;
+                Scanner input = new Scanner(System.in);
+                System.out.print("Enter  data: ");
+                data = input.nextFloat();
+                System.out.print(data + "\n");
+            } catch (Exception e) {
+                System.out.print("Enter data double value!\n");
+            }
+        }
+    }
 }
--- src/Lesson2/Seminare2.java
+++ src/Lesson2/Seminare2.java
@@ -1,4 +1,62 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Seminare2 {
 
+    public static List<String[]> myReader(String filepath) {
+        try {
+            FileReader fr = new FileReader(filepath);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            List<String[]> reader = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                String[] r = line.split("=");
+                reader.add(r);
+            }
+            return reader;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<String[]> checkArray(List<String[]> list) {
+
+        for (String[] strings : list) {
+            if (strings[1].equals("?")) {
+                strings[1] = Integer.toString(strings[0].length());
+            }
+        }
+
+        return list;
+    }
+
+    public static void myWriter(List<String[]> list, String filepath) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(filepath));
+            for (String[] strings : list) {
+                bw.write(strings[0] + "=" + strings[1] + "\n");
+            }
+            bw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String filepath = "test.txt";
+        List<String[]> l = myReader(filepath);
+        myWriter(checkArray(l), filepath);
+        // for (String[] strings : checkArray(l)) {
+        // System.out.println(strings[0] + "=" + strings[1] + "\n");
+        // }
+    }
 }
